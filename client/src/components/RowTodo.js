8107cb07e88dee31204dba8a3287f246010c90cf
Ted <marozzi.t@gmail.com>
2021-04-22 15:43:09 +1000

    Format

--- client/src/components/RowTodo.js
+++ client/src/components/RowTodo.js
@@ -2,13 +2,11 @@ import React, { Fragment } from "react";
 import "./RowTodo.css";
 
 const RowTodo = ({ todo }) => {
-
   const updateChecked = async () => {
-
     const checked = !todo.checked;
     try {
       const body = { checked };
-      
+
       await fetch(`http://localhost:5000/todos/${todo.todo_id}/updatechecked`, {
         method: "PUT",
         headers: { "Content-Type": "application/json" },
@@ -16,28 +14,20 @@ const RowTodo = ({ todo }) => {
       });
 
       window.location = "/";
-    
     } catch (err) {}
+  };
 
-  
-  }
-  
   return (
     <Fragment>
       <td>
         <span onClick={updateChecked}>
-        
-          <input
-      
-            type="checkbox"
-            readOnly={true}
-            checked={todo.checked}
-            
-          />
+          <input type="checkbox" readOnly={true} checked={todo.checked} />
           <span className="hide align-middle"></span>
         </span>
       </td>
-      <td className= {(todo.checked ? "checked" : "none") + "  align-middle"}>{todo.description}</td>
+      <td className={(todo.checked ? "checked" : "none") + "  align-middle"}>
+        {todo.description}
+      </td>
     </Fragment>
   );
 };
